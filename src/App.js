11cf82c875c2bd519f1ac04a823a45959a2223b7
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2022-11-04 20:56:05 +0530

    Added hot toasts

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { useSelector } from "react-redux";
 import Loading from "./pages/Loading";
+import toast, { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 
 const App = () => {
-  const { isLoading } = useSelector((state) => state.se);
+  const { isLoading, isError, errorMessage } = useSelector((state) => state.se);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(errorMessage, {
+        duration: 1000,
+      });
+    }
+  }, [errorMessage, isError]);
 
   if (isLoading) return <Loading />;
 
@@ -17,6 +27,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <Toaster />
     </div>
   );
 };
